afad9d9f6143dd129b212d6e87af68c35adafc84
dimejiconsult <webadmin@dimejiconsult.com>
2020-07-14 17:14:40 +0100

    update backend

--- server/UserProfile/models.py
+++ server/UserProfile/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractUser
+from safedelete.models import SafeDeleteModel
+from safedelete.models import SOFT_DELETE_CASCADE
+from safedelete.models import SafeDeleteManager
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    pass
+
+class Profile(SafeDeleteModel, AbstractUser):
+    pass
+
