3b6604164d20491d40391cddb9dd16610b2333ab
dimejiconsult <webadmin@dimejiconsult.com>
2020-08-04 22:50:16 +0000

    home page added

--- server/Homepage/views.py
+++ server/Homepage/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-
+import datetime
+from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, 'home.html')
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
